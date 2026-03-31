c1b8985010c70698eb0fbc9d04e00344f2bd5f2e
Zachary <160263126+Zitkq@users.noreply.github.com>
2026-03-31 19:34:38 -0400

    fixed the darkmode for settings and student dashboard

--- Frontend/scripts/dark.js
+++
@@ -1,6 +0,0 @@
-src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"
-
-function addDarkmodeWidget() {
-    new Darkmode().showWidget();
-  }
-window.addEventListener('load', addDarkmodeWidget);
